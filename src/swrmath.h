41dae70cba71196d59718f26003d281215e8052e
PJayB <mygit@pjblewis.com>
2015-10-27 14:55:43 -0700

    static inline warning fixes

--- src/swrmath.h
+++ src/swrmath.h
@@ -4,22 +4,22 @@
 extern "C" {
 #endif
 
-inline void vec3_copy(vec3 out, const vec3 in) {
+static inline void vec3_copy(vec3 out, const vec3 in) {
     out[0] = in[0];
     out[1] = in[1];
     out[2] = in[2];
 }
-inline void vec3_floor(vec3 out, const vec3 in) {
+static inline void vec3_floor(vec3 out, const vec3 in) {
     out[0] = fix16_floor(in[0]);
     out[1] = fix16_floor(in[1]);
     out[2] = fix16_floor(in[2]);
 }
 
-inline void vec2_copy(vec2 out, const vec2 in) {
+static inline void vec2_copy(vec2 out, const vec2 in) {
     out[0] = in[0];
     out[1] = in[1];
 }
-inline void vec2_floor(vec2 out, const vec2 in) {
+static inline void vec2_floor(vec2 out, const vec2 in) {
     out[0] = fix16_floor(in[0]);
     out[1] = fix16_floor(in[1]);
 }
