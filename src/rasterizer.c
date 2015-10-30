4800e87ad8ff75e5861ab95c96951f28234c2068
Pete Lewis <mygit@pjblewis.com>
2015-10-30 08:03:31 -0700

    Fleshing out the rasterizer API

---
+++ src/rasterizer.c
@@ -0,0 +1,7 @@
+#include "common.h"
+#include "rasterizer.h"
+#include "holomesh.h"
+#include "facesort.h"
+
+
+
---
+++ src/rasterizer.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _face_sort_key face_sort_key;
+
+typedef struct _stepping_edge {
+    fix16_t x, step_x;
+    fix16_t z, step_z;
+    fix16_t u, step_u;
+    fix16_t v, step_v;
+} stepping_edge;
+
+typedef struct _stepping_span {
+    stepping_edge e0, e1;
+    struct _stepping_span* next_span;
+    uint8_t y0, y1;
+    uint8_t texture;
+} stepping_span;
+
+typedef struct _rasterizer_context {
+    const vec3* points;
+    face_sort_key* face_kickoffs;
+    stepping_span span_pool;
+    size_t span_pool_size;
+    size_t face_kickoffs_count;
+    fix16_t* scanline_depth_buffer;
+    void* user_ptr;
+    // todo: hulls, textures
+} rasterizer_context;
+
+// Construct face kickoffs and spans from hulls
+// TODO
+
+// Do the rasterization
+void rasterize(rasterizer_context* context);
+
+#ifdef __cplusplus
+}
+#endif
+
