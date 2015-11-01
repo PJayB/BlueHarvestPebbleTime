4e59d303803fdbed9656651f958f6cbcf904fe8c
Pete Lewis <mygit@pjblewis.com>
2015-11-01 14:09:06 -0800

    Render wireframe utility func

--- src/render.c
+++ src/render.c
@@ -3,6 +3,7 @@
 #include "holomesh.h"
 #include "render.h"
 #include "matrix.h"
+#include "wireframe.h"
 
 #ifdef RASTERIZER_CHECKS
 static size_t g_active_span_high_watermark = 0;
@@ -31,6 +32,26 @@ void render_transform_hull(const holomesh_hull_t* hull, const matrix_t* transfor
         p.y = fix16_floor(fix16_add(fix16_mul(p.y, viewport_half_height), viewport_half_height));
 
         transformed_vertices[i] = p;
+    }
+}
+
+void render_draw_hull_wireframe(void* user_ptr, const holomesh_hull_t* hull, const vec3_t* transformed_vertices) {
+    wireframe_context ctx;
+    ctx.edge_indices = (const uint8_t*) hull->edges.ptr;
+    ctx.num_edges = hull->edges.size;
+    ctx.points = (vec3_t*) transformed_vertices;
+    ctx.user_ptr = user_ptr;
+    wireframe_draw(&ctx);
+}
+
+void render_draw_mesh_wireframe(void* user_ptr, const viewport_t* viewport, const holomesh_t* mesh, const matrix_t* transform, vec3_t* transformed_vertices) {
+    fix16_t half_width = viewport->fwidth >> 1;
+    fix16_t half_height = viewport->fheight >> 1;
+
+    for (size_t i = 0; i < mesh->hulls.size; ++i) {
+        const holomesh_hull_t* hull = &mesh->hulls.ptr[i];
+        render_transform_hull(hull, transform, half_width, half_height, transformed_vertices);
+        render_draw_hull_wireframe(user_ptr, hull, transformed_vertices);
     }
 }
 
--- src/render.h
+++ src/render.h
@@ -7,6 +7,8 @@ extern "C" {
 void render_init(void);
 void render_create_3d_transform(matrix_t* out, const matrix_t* proj, fix16_t angle);
 void render_transform_hull(const holomesh_hull_t* hull, const matrix_t* transform, fix16_t viewport_half_width, fix16_t viewport_half_height, vec3_t* transformed_vertices);
+void render_draw_hull_wireframe(void* user_ptr, const holomesh_hull_t* hull, const vec3_t* transformed_vertices);
+void render_draw_mesh_wireframe(void* user_ptr, const viewport_t* viewport, const holomesh_t* mesh, const matrix_t* transform, vec3_t* transformed_vertices);
 
 #ifdef __cplusplus
 }
