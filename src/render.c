9644f5c3060683a87f7dc04fe1a867ebd7c6ff62
Pete Lewis <mygit@pjblewis.com>
2015-11-01 14:00:49 -0800

    Transform the mesh in C

--- src/render.c
+++ src/render.c
@@ -21,7 +21,18 @@ void render_create_3d_transform(matrix_t* out, const matrix_t* proj, fix16_t ang
     matrix_multiply(out, &rotation, proj);
 }
 
+void render_transform_hull(const holomesh_hull_t* hull, const matrix_t* transform, fix16_t viewport_half_width, fix16_t viewport_half_height, vec3_t* transformed_vertices) {
+    for (size_t i = 0; i < hull->vertices.size; ++i) {
+        vec3_t p = *((vec3_t*) &hull->vertices.ptr[i]);
+        matrix_vector_transform(&p, transform);
 
+        // Move to NDC
+        p.x = fix16_floor(fix16_add(fix16_mul(p.x, viewport_half_width), viewport_half_width));
+        p.y = fix16_floor(fix16_add(fix16_mul(p.y, viewport_half_height), viewport_half_height));
+
+        transformed_vertices[i] = p;
+    }
+}
 
 #ifdef RASTERIZER_CHECKS
 size_t render_get_span_high_watermark(void) {
--- src/render.h
+++ src/render.h
@@ -6,6 +6,7 @@ extern "C" {
 
 void render_init(void);
 void render_create_3d_transform(matrix_t* out, const matrix_t* proj, fix16_t angle);
+void render_transform_hull(const holomesh_hull_t* hull, const matrix_t* transform, fix16_t viewport_half_width, fix16_t viewport_half_height, vec3_t* transformed_vertices);
 
 #ifdef __cplusplus
 }
