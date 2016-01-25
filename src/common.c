b58b04d5d7b53a07deac85c050136fcb5113b7de
Pete Lewis <mygit@pjblewis.com>
2016-01-24 20:05:11 -0800

    Assert seems to have broken again, so here's a better one

--- src/common.c
+++ src/common.c
@@ -9,13 +9,10 @@ void viewport_init(viewport_t* viewport, uint16_t width, uint16_t height) {
 
 #ifdef PEBBLE
 void assert(const char* file, int line, const char* condition) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "ASSERT: %s(%d): %s", file, line, condition);
-
-    // Hang for 10 seconds (so logs can finish transferring)
-    psleep(10000);
-    
-    // Crash
-    *((uint32_t*)0) = 0;
+    while (1) {
+        APP_LOG(APP_LOG_LEVEL_ERROR, "ASSERT: %s(%d): %s", file, line, condition);
+        psleep(1000);
+    }
 }
 #endif
 
